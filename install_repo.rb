9cc7882894e6dd386bde0831eb35f62987b166df
Niklaus Giger <niklaus.giger@member.fsf.org>
2015-12-19 21:55:34 +0100

    Simplified install_repo.rb

--- install_repo.rb
+++ install_repo.rb
@@ -7,6 +7,9 @@ require "find"
 require "fileutils"
 require 'optparse'  # This hash will hold all of the options # parsed from the command-line by # OptionParser.
 require 'pp'
+
+VALID_VARIANTS = ['snapshot', 'beta', 'prerelease', 'release']
+
 options = {}
 repo_name = File.basename(`git config --get remote.origin.url`.chomp, '.git').gsub('-', '.')
 dest = ENV['ROOT']
@@ -15,65 +18,45 @@ optparse = OptionParser.new do |opts|
   opts.banner = "Usage: install_repo.rb path_to_repo. Defaults to #{dest}
   or the value of the ROOT environment variable"
   options[:variant] = 'snapshot'
-  opts.on('--variant', 'Must be one of snapshot, beta, prerelease, release. Defaults to snapshot' ) do |value|
-    unless ['snapshot', 'beta', 'prerelease', 'release'].index(value)
+  opts.on('--variant variant', 'Must be one of snapshot, beta, prerelease, release. Defaults to snapshot' ) do |value|
+    unless VALID_VARIANTS.index(value)
         puts optparse
         exit 1
     end
     options[:variant] = value
   end
-  options[:try] = false
-  opts.on('--try', 'Move actual snapshot to snapshot_before_try' ) do
-    options[:try] = true
-  end
-  options[:install] = false
-  opts.on('--install', 'Move the snapshot_before_try to backup directory' ) do
-    options[:install] = true
-  end
-  options[:revert] = false
-  opts.on('--revert', 'Revert to snapshot_before_try' ) do
-    options[:revert] = true
-  end
 end
 optparse.parse!
-if ARGV.size > 1 || (options[:install] == false &&
-                     options[:revert] == false &&
-                     options[:try] == false)
-  puts optparse
-  exit 3
-end
 repository = Dir.glob('**/target/repository')
 unless repository.size == 1
   puts "Abort as no target/repository found"
   exit 1
 end
 repository = repository.first
+
+variant = options[:variant]
+
+# Variant may be overridden by value repoVariant from repo.properties file
+properties = File.join(File.dirname(File.dirname(repository)), 'repo.properties')
+if File.exist?(properties)
+  inhalt = IO.read(properties)
+  m = /^repoVariant=(.+)$/.match(inhalt)
+  variant = m[1] if m && VALID_VARIANTS.index(m[1])
+end
+
 timestamp = File.mtime(repository).strftime('%Y%m%d-%H%M')
 dest = ARGV[0] if ARGV[0]
-published = "#{dest}/#{options[:variant]}"
-before = "#{dest}/#{options[:variant]}_before_try"
+published = "#{dest}/#{variant}"
+before = "#{dest}/#{variant}_before_try"
+backup = "#{dest}/backups/#{variant}"
 puts "published #{published} dest #{dest} before #{before}"
-if options[:try]
-  FileUtils.mv(published, before, :verbose => true) unless File.exist?(before)
-  FileUtils.cp_r(repository, published, :verbose => true, :preserve => true)
-  exit 0
-end
-backup = "#{dest}/backups/#{options[:variant]}"
-if options[:revert]
-  unless File.exist?(before)
-    puts "Cannot revert as #{before} not found.\n   Maybe your are lucky and find an old version in #{backup}"
-    exit 3
-  end
-  FileUtils.rm_rf(published, :verbose => true, :preserve => true)
-  FileUtils.mv(before, published, :verbose => true) unless File.exist?(before)
-  exit 0
-end
-if options[:install]
-  unless File.exist?(before)
-    puts "Nothing to do. No #{before} found"
-    exit 0
-  end
-  timestamp = File.mtime(before).strftime('%Y%m%d-%H%M')
+
+if File.exist?(published)
+  timestamp = File.mtime(published).strftime('%Y%m%d-%H%M')
   FileUtils.makedirs(backup)
-  FileUtils.mv(before, "#{backup}/#{timestamp}", :verbose => true) if File.exist?(before)
+  target = "#{backup}/#{timestamp}"
+  FileUtils.mv(published, "#{target}", :verbose => true) unless File.exist?(target)
 end
+FileUtils.makedirs(File.dirname(published))
+FileUtils.cp_r(repository, published, :verbose => true, :preserve => true)
+
